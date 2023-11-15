5ca6b05ad5beb7fc28b2ba3539d27df8706377ad
BuildTools <unconfigured@null.spigotmc.org>
2023-11-14 21:12:46 -0600

    Initial commit

---
+++ src/main/java/saday/underground/UndergroundClient.java
@@ -0,0 +1,12 @@
+package saday.underground;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UndergroundClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
